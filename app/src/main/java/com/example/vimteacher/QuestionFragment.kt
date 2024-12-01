1bd675d2458b362ae0c0c3c58aad4cc109d1ccd7
Ata Tan Dagidir <tan.dagidir@ozu.edu.tr>
2024-12-01 22:49:06 +0300

    added some features to question fragment

--- app/src/main/java/com/example/vimteacher/QuestionFragment.kt
+++ app/src/main/java/com/example/vimteacher/QuestionFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.RadioButton
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -34,18 +38,36 @@ class QuestionFragment : Fragment() {
         viewModel.setQuestionById(args.id)
         viewModel.currentQuestionLiveData.observe(viewLifecycleOwner){ question ->
             binding.questionId.id = question.questionId
+            binding.radioGroup.removeAllViews()
+            val container = binding.explanationsCard.findViewById<LinearLayout>(R.id.explanationsContainer)
+            container.removeAllViews()
             question.options.forEach{ option ->
-                val radioButton = RadioButton(requireContext())
-                radioButton.text = option.optionBody
+                val radioButton = RadioButton(requireContext()).apply {
+                    text = option.optionBody
+                    setTextColor(ResourcesCompat.getColorStateList(resources, R.color.radio_button_text_color, null))
+                    buttonTintList = ResourcesCompat.getColorStateList(resources, R.color.radio_button_text_color, null)
+                }
                 binding.radioGroup.addView(radioButton)
+                val explanationView = TextView(requireContext()).apply {
+                    text = option.optionDescription
+                    textSize = 16f
+                    setPadding(16, 8, 16, 8)
+                    layoutParams = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+                }
+                container.addView(explanationView)  // Add to container instead of CardView
             }
+
             binding.questionBody.text = question.questionBody
         }
-//        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
-//            if (question != null && checkedId == question.questionId) {
-//                TODO("TO BE CONTINUED IN PART 2")
-//            }
-//        }
+
+        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            question?.options?.forEach{ option ->
+                binding.explanationsCard.visibility = View.VISIBLE
+            }
+        }
     }
 
 
